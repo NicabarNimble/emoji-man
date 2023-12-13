e060175322b497b13f13b7bceb39f8247ba33d9d
NicabarNimble <nicabar@gmail.com>
2023-12-12 22:31:31 -0500

    fixed enviro

--- contracts/scripts/default_auth.sh
+++ contracts/scripts/default_auth.sh
@@ -6,29 +6,30 @@ pushd $(dirname "$0")/..
 mode=${1:-dev} # default to 'dev' if no argument is provided
 
 # Use different manifest file based on the mode
-if [ "$mode" = "release" ]; then
-    manifest_file="./target/release/manifest.json"
-    export RPC_URL="https://api.cartridge.gg/x/emoji-man-demo/katana";
+if [[ ${mode} == "release" ]]; then
+	manifest_file="./target/release/manifest.json"
+	export RPC_URL="https://api.cartridge.gg/x/emoji-man-demo/katana"
 else
-    manifest_file="./target/dev/manifest.json"
-    export RPC_URL="http://localhost:5050";
+	manifest_file="./target/dev/manifest.json"
+	export RPC_URL="http://localhost:5050"
 fi
 
-export WORLD_ADDRESS=$(cat $manifest_file | jq -r '.world.address')
+export WORLD_ADDRESS=$(cat "${manifest_file}" | jq -r '.world.address')
 
-export ACTIONS_ADDRESS=$(cat $manifest_file | jq -r '.contracts[] | select(.name == "actions" ).address')
+export ACTIONS_ADDRESS=$(cat "${manifest_file}" | jq -r '.contracts[] | select(.name == "actions" ).address')
 
 echo "---------------------------------------------------------------------------"
-echo world : $WORLD_ADDRESS 
+echo world : "${WORLD_ADDRESS}"
 echo " "
-echo actions : $ACTIONS_ADDRESS
+echo actions : "${ACTIONS_ADDRESS}"
 echo "---------------------------------------------------------------------------"
 
 # enable system -> component authorizations
-COMPONENTS=("Position" "MovesQueue" "RPSType" "GameData" "PlayerID" "Energy" "PlayerAddress" "PlayerAtPosition" )
+COMPONENTS=("Position" "MovesQueue" "RPSType" "GameData" "PlayerID" "Energy" "PlayerAddress" "PlayerAtPosition")
 
-for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component $ACTIONS_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+for component in "${COMPONENTS[@]}"; do
+	sozo auth writer "${component}" "${ACTIONS_ADDRESS}" --world "${WORLD_ADDRESS}" --rpc-url "${RPC_URL}"
 done
 
 echo "Default authorizations have been successfully set."
+echo "${RPC_URL}"
